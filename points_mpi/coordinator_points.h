7f36b81203f247812dcc69252bcac78a9edc6eeb
Evan Palmer <evansp01@gmail.com>
2014-12-03 19:47:26 -0500

    renamed many files

--- points_mpi/coordinator_points.h
+++
@@ -1,6 +0,0 @@
-#ifndef COORDINATOR_POINTS_   /* Include guard */
-#define COORDINATOR_POINTS_
-
-void main_routine(int world_size, char *infile, char *outfile, int iterations, int clusters);
-
-#endif
--- points_mpi/participant_points.h
+++
@@ -1,6 +0,0 @@
-#ifndef PARTICIPANT_POINTS_   /* Include guard */
-#define PARTICIPANT_POINTS_
-
-void worker_routine(int rank);
-
-#endif
